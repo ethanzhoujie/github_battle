a7c15ad51623458d9138b775152a866cb6f60807
Ethan Zhou <ethan.zhoujie@gmail.com>
2020-10-31 14:47:02 -0400

    14-api

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { battle } from '../utils/api'
 
 export default class Results extends React.Component {
     constructor(props) {
         super(props);
     }
+
+    componentDidMount() {
+        const { playerOne, playerTwo } = this.props
+        battle([playerOne, playerTwo]).then((players) => {
+            console.log('data', players)
+        })
+    }
+    
     render() {
         return (
             <div>
