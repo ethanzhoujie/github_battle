e61b07047d78573dff31a675aeb2f9a4bcc6ed1c
Ethan Zhou <ethan.zhoujie@gmail.com>
2020-11-03 00:12:40 -0500

    21-tooltip

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -3,7 +3,8 @@
  import { fetchPopularRepos } from '../utils/api'
  import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
  import Card from './Card'
- import Loading from "./Loading";
+ import Loading from "./Loading"
+ import Tooltip from "./Tooltip"
 
  function LanguageNav ({ selected, onUpdateLanguage}) {
      const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -45,10 +46,12 @@
                          >
                              <ul className='card-list'>
                                  <li>
-                                     <FaUser color='rgb(255, 191, 116)' size={22} />
-                                     <a href={`https://github.com/${login}`} target='_blank'>
-                                         {login}
-                                     </a>
+                                     <Tooltip text="Github username">
+                                         <FaUser color='rgb(255, 191, 116)' size={22} />
+                                         <a href={`https://github.com/${login}`} target='_blank'>
+                                             {login}
+                                         </a>
+                                     </Tooltip>
                                  </li>
                                  <li>
                                      <FaStar color='rgb(255, 215, 0)' size={22} />
