9ca6cbf60eb1cd60427a7384c003344d2f665a43
Ethan Zhou <ethan.zhoujie@gmail.com>
2020-11-09 00:13:05 -0500

    30-404

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ import Popular from "./components/Popular"
 import Battle from "./components/Battle"
 import Results from "./components/Results"
 import { ThemeProvider} from "./contexts/theme"
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // Component
 // State
@@ -33,10 +33,12 @@ class App extends React.Component {
                     <div className={this.state.theme}>
                         <div className='container'>
                             <Nav />
-
-                            <Route exact path='/' component={Popular} />
-                            <Route exact path='/battle' component={Battle} />
-                            <Route path='/battle/results' component={Results} />
+                            <Switch>
+                                <Route exact path='/' component={Popular} />
+                                <Route exact path='/battle' component={Battle} />
+                                <Route path='/battle/results' component={Results} />
+                                <Route render={() => <h1>404</h1>} />
+                            </Switch>
                         </div>
                     </div>
                 </ThemeProvider>
